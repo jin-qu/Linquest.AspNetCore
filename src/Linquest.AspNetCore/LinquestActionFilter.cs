c75e9593d7e127d485122e934ef234804cfd532f
Umut Özel <umutozel@outlook.com>
2018-09-09 12:15:09 +0300

    create action filter

--- src/Linquest.AspNetCore/LinquestActionFilter.cs
+++ src/Linquest.AspNetCore/LinquestActionFilter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,21 +12,40 @@ namespace Linquest.AspNetCore {
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
     public class LinquestActionFilterAttribute : ActionFilterAttribute {
 
-        public LinquestActionFilterAttribute(Type configType = null): base() {
+        public LinquestActionFilterAttribute() : base() {
             Order = 0;
-            
-            if (configType == null) return;
-
-            Config = Activator.CreateInstance(configType) as ILinquestConfig;
-            if (Config == null) throw new ArgumentException("Cannot create config instance");
         }
-
-        public LinquestActionFilterAttribute(ILinquestConfig config) => Config = config;
-
-        public ILinquestConfig Config { get; }
 
         public override void OnResultExecuting(ResultExecutingContext context) {
             base.OnResultExecuting(context);
+
+            if (!(context.Result is ObjectResult objectResult))return;
+
+            if (context.ActionDescriptor is ControllerActionDescriptor cad &&
+                cad.MethodInfo.CustomAttributes.Any(a => a.AttributeType == typeof(NonLinquestActionAttribute)))return;
+
+            var value = objectResult.Value;
+            var service = context.Controller as ILinquestService;
+            var request = context.HttpContext.Request;
+            var response = context.HttpContext.Response;
+
+            // translate the request query
+            var actionContext = new ActionContext(
+                context.ActionDescriptor, value, GetParameters(request)
+            );
+
+            var processResult = ProcessRequest(actionContext);
+            context.Result = HandleResponse(processResult, response);
         }
+
+        public virtual IList < (string, string) > GetParameters(HttpRequest request) {
+            return Helper.GetParameters(request);
+        }
+
+        public virtual ProcessResult ProcessRequest(ActionContext actionContext) {
+            return Helper.DefaultRequestProcessor(actionContext);
+        }
+
+        protected virtual ActionResult HandleResponse(ProcessResult result, HttpResponse response) => Helper.HandleResponse(result, response);
     }
 }
