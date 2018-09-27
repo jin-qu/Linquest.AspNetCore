b2efa724f8b4f8e3148a7305ffcb243ddf7c2289
Umut Özel <umutozel@outlook.com>
2018-09-27 13:13:43 +0300

    add tests

--- test/Linquest.AspNetCore.Tests/QueryTests.cs
+++ test/Linquest.AspNetCore.Tests/QueryTests.cs
@@ -35,6 +35,34 @@ namespace Linquest.AspNetCore.Tests {
             }
         }
 
+        [Fact]
+        public async Task ShouldGetPageData() {
+            using (var testServer = CreateTestServer()) {
+                var client = testServer.CreateClient();
+                var url = "api/Test/Orders?$skip=2&$take=2";
+                var response = await client.GetStringAsync(url);
+                var orders = JsonConvert.DeserializeObject<List<Order>>(response);
+
+                Assert.Equal(2, orders.Count);
+                Assert.Equal(3, orders[0].Id);
+                Assert.Equal("Ord4", orders[1].No);
+            }
+        }
+
+        [Fact]
+        public async Task ShouldSelectDetails() {
+            using (var testServer = CreateTestServer()) {
+                var client = testServer.CreateClient();
+                var url = $"api/Test/Orders?$selectMany={WebUtility.UrlEncode("o => o.OrderDetails")}";
+                var response = await client.GetStringAsync(url);
+                var details = JsonConvert.DeserializeObject<List<OrderDetail>>(response);
+
+                Assert.Equal(16, details.Count);
+                Assert.Equal(36, details[4].Count);
+                Assert.Equal("Prd6", details[12].Product);
+            }
+        }
+
         private TestServer CreateTestServer() {
             var builder = new WebHostBuilder()
                 .UseStartup<Startup>();
