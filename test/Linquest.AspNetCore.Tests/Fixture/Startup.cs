547fac592b7448959d8da518604af74fae101f88
Mustafa SADEDİL <sadedil@yahoo.com>
2019-10-04 23:05:26 +0300

    Add ASP.NET Core 3.0 bootstrapping logic to Startup

--- test/Linquest.AspNetCore.Tests/Fixture/Startup.cs
+++ test/Linquest.AspNetCore.Tests/Fixture/Startup.cs
@@ -5,25 +5,46 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Linquest.AspNetCore.Tests.Fixture {
+namespace Linquest.AspNetCore.Tests.Fixture
+{
     using Interface;
     using Model;
 
-    public class Startup : StartupBase {
-
+    public class Startup : StartupBase
+    {
         public IConfiguration Configuration { get; }
 
-        public Startup(IConfiguration configuration) {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
-        public override void ConfigureServices(IServiceCollection services) {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+#if NETCOREAPP2_1
             services.AddMvc();
             services.AddMvc().AddApplicationPart(typeof(Startup).Assembly);
-
+#elif NETCOREAPP3_0
+            services.AddControllers();
+#endif
             services.AddSingleton<IContentHandler<Product>, ProductHandler>();
         }
 
-        public override void Configure(IApplicationBuilder app) => app.UseMvc();
+        public override void Configure(IApplicationBuilder app)
+        {
+#if NETCOREAPP2_1
+            app.UseMvc();
+#elif NETCOREAPP3_0
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                
+                endpoints.MapControllers();
+            });
+#endif
+        }
     }
 }
